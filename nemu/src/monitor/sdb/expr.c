901b9828a92ce97ab7b6417cc688b7fd3b11ef6a
tracer-ics2023 <tracer@njuics.org>
2023-10-11 22:22:34 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:22:34 up  1:16,  1 user,  load average: 0.86, 0.69, 0.71

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -97,8 +97,8 @@ static bool make_token(char *e) {
         int substr_len = pmatch.rm_eo;
 
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        //     i, rules[i].regex, position, substr_len, substr_len, substr_start);
         
         position += substr_len;
 
@@ -115,7 +115,7 @@ static bool make_token(char *e) {
           case NUM:{
             memset(tok.str, '\0', sizeof(tok.str));
             strncpy(tok.str,substr_start,substr_len);
-            printf("Debug:tok.str:%s\n",tok.str);
+            // printf("Debug:tok.str:%s\n",tok.str);
             break;
           }
           default: {tok.str[0] = '\0';}
