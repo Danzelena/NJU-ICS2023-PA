26299a9f7afe9408a8550a3e5c835a8dc857a468
tracer-ics2023 <tracer@njuics.org>
2023-10-13 15:00:56 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:00:56 up  1:18,  1 user,  load average: 0.87, 0.51, 0.48

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -456,7 +456,7 @@ u_int32_t eval(Token *tokens, int begin, int end)
 bool isCertainType(int type)
 {
   if (type == '+' || type == '-' || type == '*' || type == '/' 
-  ||type == '('||type == ')'|| type == NEG||
+  || type == NEG||
    type == TK_EQ || type == TK_NEQ || type == AND || type == OR)
   {
     return true;
