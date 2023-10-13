eb4ec240634d4f1c8f7fa3b3c6a28fb6d8935f28
tracer-ics2023 <tracer@njuics.org>
2023-10-13 14:06:34 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:06:34 up 23 min,  1 user,  load average: 0.87, 0.86, 0.67

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -56,9 +56,9 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
-  nemu_state.state = NEMU_END;
-  cpu_exec(-1);
-  return 0;//'q' for quit
+  nemu_state.state = NEMU_QUIT;
+  // cpu_exec(-1);
+  return -1;//'q' for quit
 }
 
 static int cmd_help(char *args);
