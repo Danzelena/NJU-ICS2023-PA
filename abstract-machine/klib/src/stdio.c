6f9a5f065d3fce85350a50d17e34668eb9f8836b
tracer-ics2023 <tracer@njuics.org>
2023-11-04 21:16:16 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:16:16 up  1:43,  1 user,  load average: 1.25, 1.08, 0.87

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,7 +4,9 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+#define LEN 10
 
+char *itoa();
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +16,41 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  int d;
+  char c;
+  char *s;
+
+  va_start(ap,fmt);
+  while(*fmt){
+    switch (*fmt)
+    {
+    case 's':
+      s = va_arg(ap,char*);
+      out = strcat(out,s);
+      break;
+    case 'd':
+      char snum[LEN];
+      
+      d = va_arg(ap, int);
+      itoa(d,snum);
+      out = strcat(out,snum);
+
+      break;
+
+    case 'c':
+    // need a cast here becase va_arg only takes fully promoted types
+      c = (char)va_arg(ap, int);
+      out = strcat(out,&c);
+      break;
+    default:
+      break;
+    }
+    fmt++;
+  }
+  va_end(ap);
+  return 0;
+  // panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -25,4 +61,28 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+// dec for common use
+char* itoa(int val,char *str){
+  char* ret = str;
+  if(val < 0){
+    *str = '-';
+    str ++;
+    val = -val;
+  }
+  int num = val;
+  int cnt = -1;
+    while(num >0){
+    num /= 10;
+    cnt ++;
+    }
+    int len = cnt;
+  while(cnt >=0){
+    *(str+cnt) = '0' + val%10;
+    cnt--; 
+    val /= 10;
+  }
+//   *str = '0' + val%10;
+  *(str + len + 1) = '\0';
+  return ret;
+}
 #endif
