ef13b6529ca095a09441c75550784f807653ca9a
tracer-ics2023 <tracer@njuics.org>
2023-11-04 21:22:36 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:36 up  1:49,  1 user,  load average: 0.60, 0.71, 0.77

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,43 +7,57 @@
 #define LEN 10
 
 char *itoa();
-int printf(const char *fmt, ...) {
+int printf(const char *fmt, ...)
+{
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+int sprintf(char *out, const char *fmt, ...)
+{
   va_list ap;
   int d;
   char c;
   char *s;
 
-  va_start(ap,fmt);
-  while(*fmt){
+  va_start(ap, fmt);
+  while (*fmt)
+  {
     switch (*fmt)
     {
-    case 's':
-      s = va_arg(ap,char*);
-      out = strcat(out,s);
-      break;
-    case 'd':
-      char snum[LEN];
-      
-      d = va_arg(ap, int);
-      itoa(d,snum);
-      out = strcat(out,snum);
+    case '%':
+      switch (*(fmt + 1))
+      {
+      case 's':
+        s = va_arg(ap, char *);
+        out = strcat(out, s);
+        out += strlen(s);
+        break;
+      case 'd':
+        char snum[LEN];
 
+        d = va_arg(ap, int);
+        itoa(d, snum);
+        out = strcat(out, snum);
+        out += strlen(snum);
+        break;
+
+      case 'c':
+        // need a cast here becase va_arg only takes fully promoted types
+        c = (char)va_arg(ap, int);
+        out = strcat(out, &c);
+        out += 1;
+        break;
+      }
+      fmt++;
       break;
 
-    case 'c':
-    // need a cast here becase va_arg only takes fully promoted types
-      c = (char)va_arg(ap, int);
-      out = strcat(out,&c);
-      break;
     default:
+      *out = *fmt;
       break;
     }
     fmt++;
@@ -53,35 +67,41 @@ int sprintf(char *out, const char *fmt, ...) {
   // panic("Not implemented");
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
   panic("Not implemented");
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
   panic("Not implemented");
 }
 
 // dec for common use
-char* itoa(int val,char *str){
-  char* ret = str;
-  if(val < 0){
+char *itoa(int val, char *str)
+{
+  char *ret = str;
+  if (val < 0)
+  {
     *str = '-';
-    str ++;
+    str++;
     val = -val;
   }
   int num = val;
   int cnt = -1;
-    while(num >0){
+  while (num > 0)
+  {
     num /= 10;
-    cnt ++;
-    }
-    int len = cnt;
-  while(cnt >=0){
-    *(str+cnt) = '0' + val%10;
-    cnt--; 
+    cnt++;
+  }
+  int len = cnt;
+  while (cnt >= 0)
+  {
+    *(str + cnt) = '0' + val % 10;
+    cnt--;
     val /= 10;
   }
-//   *str = '0' + val%10;
+  //   *str = '0' + val%10;
   *(str + len + 1) = '\0';
   return ret;
 }
