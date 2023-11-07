3dd76a01dc59308e15fbfca110bf8f2b2a2a7cb5
tracer-ics2023 <tracer@njuics.org>
2023-11-07 23:20:51 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:20:51 up 26 min,  1 user,  load average: 0.47, 0.27, 0.23

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -13,7 +13,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 #TODO:add something so that NEMU can read ELF file
-# NEMUFLAGS += -a $(IMAGE)
+NEMUFLAGS += -a $($(IMAGE).elf)/
 # $(warning $(IMAGE))
 
 
