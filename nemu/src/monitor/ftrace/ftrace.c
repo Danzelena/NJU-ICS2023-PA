c509ae7bb36fabcb42151e4a7e488cd715d028f5
221300089-Zhang Lingxiang <elenadanz@outlook.com>
2023-11-12 00:17:54 +0800

    almost finish pa2.2

--- nemu/src/monitor/ftrace/ftrace.c
+++ nemu/src/monitor/ftrace/ftrace.c
@@ -1,10 +1,16 @@
 #include "elfloader.h"
 
-int cnt = 0;
+// int cnt = 0;
+// char *lastfunc;
+// TODO:simluate a function call stack to print
 void ftrace_call(word_t pc){
-    for(int i = 0;i < cnt;i++){
-        printf(" ");
-    }
-    printf("%s\n",gen_func_name(pc));
+    // for(int i = 0;i < cnt;i++){
+    //     printf(" ");
+    // }
+    char *func = gen_func_name(pc);
+    
+    printf("%s\n",func);
+    // if()
+    
 
 }
