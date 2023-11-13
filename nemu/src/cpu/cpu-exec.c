872e19522214d6f94e40964da6973c40e0882760
tracer-ics2023 <tracer@njuics.org>
2023-11-13 21:28:54 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:54 up  1:19,  3 users,  load average: 1.50, 0.99, 0.87

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -219,7 +219,8 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
+          irbuf_print(&irbuf);
       // fall through
-    case NEMU_QUIT: irbuf_print(&irbuf);statistic();
+    case NEMU_QUIT: statistic();
   }
 }
