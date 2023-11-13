7b6d921024ca9842a1ac134f54724abe862dab01
tracer-ics2023 <tracer@njuics.org>
2023-11-13 22:03:22 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:03:22 up  1:54,  3 users,  load average: 1.35, 0.90, 0.85

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,6 +17,11 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+const char *regs_native[] = {
+    "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+    "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+    "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 //*ref_r like: {gpr = {0, 0, 0, 0, 0, 2147483648, 0 <repeats 26 times>}, pc = 2147483652}
 // pc : 2147483648
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
@@ -24,6 +29,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int reg_num = 32;
   for(i = 0;i < reg_num;i++){
     if((*ref_r).gpr[i] != cpu.gpr[i]){
+      printf("Fault:register %s",regs_native[i]);
       return false;
     }
   }
