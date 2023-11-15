87c850bcd1f4fd96d27fd8693e8a262c7ea1d1db
tracer-ics2023 <tracer@njuics.org>
2023-11-15 22:02:35 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:02:35 up  2:28,  1 user,  load average: 0.99, 1.02, 0.94

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -9,7 +9,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = (uint64_t)((uint64_t)(RTC_ADDR + 4)<< 32) | (uint64_t)(RTC_ADDR);
-  printf("us: %lu\n",(unsigned long)uptime->us);
+  printf("us: %d\n",(int)uptime->us);
   // uptime->us = 0;
 }
 
