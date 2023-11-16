2e3ef8cf6cba20df4d3b6f1719f96cfb3d8ab48f
tracer-ics2023 <tracer@njuics.org>
2023-11-16 20:48:21 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:48:21 up  2:13,  3 users,  load average: 0.78, 0.85, 0.88

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -55,9 +55,15 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 
 /* bus interface */
 word_t mmio_read(paddr_t addr, int len) {
-  return map_read(addr, len, fetch_mmio_map(addr));
+  IOMap *mmap = fetch_mmio_map(addr);
+  // TODO:device trace(dtrace)
+  printf("dtrace: read %s\n", mmap->name);
+  return map_read(addr, len, mmap);
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
-  map_write(addr, len, data, fetch_mmio_map(addr));
+  IOMap *mmap = fetch_mmio_map(addr);
+  // TODO:device trace(dtrace)
+  printf("dtrace: write %s\n", mmap->name);
+  map_write(addr, len, data, mmap);
 }
