94e94eab6414d76f4f6b65afa91f186bf21f4571
tracer-ics2023 <tracer@njuics.org>
2023-11-16 21:25:54 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:25:54 up  2:51,  3 users,  load average: 1.21, 0.98, 0.86

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,5 +1,7 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
+
 
 //(uint32_t)
 //  >0x8000: keydown
@@ -9,6 +11,7 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   int key = (int32_t)inl(KBD_ADDR);
+  printf("key=%d\n", key);
   kbd->keydown = key > KEYDOWN_MASK;
   kbd->keycode = key > KEYDOWN_MASK ? (key - KEYDOWN_MASK) : key;
 //   kbd->keydown = 0;
