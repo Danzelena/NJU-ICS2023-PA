ad326f4d2fcab1cb717aa5f37306ed6545278ea9
tracer-ics2023 <tracer@njuics.org>
2023-11-17 00:21:59 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:21:59 up  1:36,  3 users,  load average: 1.61, 0.86, 0.49

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -11,340 +11,340 @@
 
 
 
-
-int int2str(char *buf, int num){
-	if (num == -2147483648){
-		strcat(buf, "-2147483648");
-		return 11;
-	}	
-	int ret = 0;
-	if(num < 0){
-		*buf++ = '-';
-		ret++;
-		num = -num;
-	}
-	char *p = buf;
-	char *q = buf;
-	do {ret++;
-		*q++ = (char)(num%10 + '0');
-		num /= 10;
-	}while(num);
-	*q = '\0';
-	q--;
-	while(q > p){
-		char temp = *p;
-		*p++ = *q;
-		*q-- = temp;
-	}
-	return ret;
-}
-
-int x2str(char *buf, unsigned num){
-	char xtab[] = "0123456789abcdef";
-	int ret = 0;
-	char *p = buf;
-	char *q = buf;
-	while(num > 15)
-	{
-		*q++ = xtab[num%16];
-		ret++;
-		num /= 16;
-	}			
-	*q++ = xtab[num%16];
-	ret++;
-	*q = '\0';
-	q--;
-	while(q > p){
-		char temp = *p;
-		*p++ = *q;
-		*q-- = temp;
-	}
-	return ret;
-}
-
-int str2str(char *buf, char *src){
-
-	strcat(buf, src);
-	return strlen(src);
-}
-
-int float2str(char *buf, float flt, int len){
-	int ret = 0;
-	if (flt < 0){
-		ret++;
-		*buf++ = '-';
-	}
-	int flt_int = (int)flt;
-	float flt_frac = flt - flt_int;
-	int tmp_ret = int2str(buf, flt_int);
-	ret += tmp_ret;
-	buf += tmp_ret;
-	ret ++;
-	*buf++ = '.';
-	while(len--){
-		flt_frac *= 10;
-	}
-	int flt_frac_int = (int)flt_frac;
-	ret += int2str(buf, flt_frac_int);
-	return ret;
-
-}
-
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-	/* panic("Not implemented"); */
-
-	*out = '\0';
-	int ret = 0;
-	while(*fmt != '\0'){
-		if(*fmt != '%'){
-			ret++;
-			*out++ = *fmt++;
-			*out = '\0';
-		}
-		else{
-			fmt++;
-			switch(*fmt){
-				case 's':{	 char *valstr = va_arg(ap, char*);
-							 int sub_ret = str2str(out, valstr);
-							 ret += sub_ret;
-							 out += sub_ret;
-							 fmt++;
-							 assert(*out=='\0');
-							 break;
-						 }
-				case 'd':{	 int valint = va_arg(ap, int);
-							 int sub_ret = int2str(out, valint);
-							 ret += sub_ret;
-							 out += sub_ret;
-							 fmt++;
-							 break;
-						 }
-				/* case 'f':{ */	 
-				/* 			 float valflt  = va_arg(ap, double); */
-				/* 			 int sub_ret = float2str(out, valflt, 6); */
-				/* 			 ret += sub_ret; */
-				/* 			 out += sub_ret; */
-				/* 			 fmt++; */
-				/* 			 /1* *out = '\0'; *1/ */
-				/* 			 break; */
-				/* 		 } */
-				case 'x':{	 
-							 int val  = va_arg(ap , int);
-							 int sub_ret = x2str(out, val);
-							 ret += sub_ret;
-							 out += sub_ret;
-							 fmt++;
-							 /* *out = '\0'; */
-							 break;
-						 }
-				case 'p':{	 
-							 ret += 2;
-							 *out++ = '0';
-							 *out++ = 'x';
-							 int val  = va_arg(ap , int);
-							 char addr[10]="\0";
-							 int sub_ret = x2str(addr, val);
-							 for(int k = 8-sub_ret; k > 0; k--){
-								*out++ = '0';
-							 }
-							 *out = '\0';
-							 strcat(out, addr);
-							 ret += 8;
-							 out += sub_ret;
-							 fmt++;
-							 /* *out = '\0'; */
-							 break;
-						 }
-				case 'c':{
-							char val = va_arg(ap, int);
-							ret++;
-							*out++ = val;
-							fmt++;
-							break;
-						 }
-				default: {	 ret++;
-							 *out++ = *fmt++;
-							 *out = '\0';
-						 }
-			}
-		}
-
-		assert(*out == '\0');
-	}
-	assert(*out == '\0');
-	return ret;
-}
-
-
-int sprintf(char *out, const char *fmt, ...) {
-	/* panic("Not implemented"); */
-	va_list ap;
-	va_start(ap, fmt);
-	int ret = vsprintf(out, fmt, ap);
-	va_end(ap);
-	return ret;
-}
-
-
-int printf(const char *fmt, ...) {
-	/* panic("Not implemented"); */
-	char out[4095];
-	va_list ap;
-	va_start(ap, fmt);
-	int ret = vsprintf(out, fmt, ap);
-	va_end(ap);
-	/* putch('A'); */
-	/* for(int k = 0; k < 20; k++){ */
-	/* putch(out[k]); */
-	/* putch('\n'); */
-	/* } */
-	putstr(out);
-	return ret;
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-	panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-	panic("Not implemented");
-}
-
-
-/*TODO:fix this biggggggg BUG!!!*/
-// int vsprintf(char *out, const char *fmt, va_list ap);
-// int vsnprintf(char *out, size_t n, const char *fmt, va_list ap);
-
-// int printf(const char *fmt, ...) {
-//   char buffer[2048];
-//   va_list arg;
-//   va_start (arg, fmt);
-  
-//   int done = vsprintf(buffer, fmt, arg);
-
-//   putstr(buffer);
-
-//   va_end(arg);
-//   return done;
+//TODO:target1
+// int int2str(char *buf, int num){
+// 	if (num == -2147483648){
+// 		strcat(buf, "-2147483648");
+// 		return 11;
+// 	}	
+// 	int ret = 0;
+// 	if(num < 0){
+// 		*buf++ = '-';
+// 		ret++;
+// 		num = -num;
+// 	}
+// 	char *p = buf;
+// 	char *q = buf;
+// 	do {ret++;
+// 		*q++ = (char)(num%10 + '0');
+// 		num /= 10;
+// 	}while(num);
+// 	*q = '\0';
+// 	q--;
+// 	while(q > p){
+// 		char temp = *p;
+// 		*p++ = *q;
+// 		*q-- = temp;
+// 	}
+// 	return ret;
 // }
 
-// static char HEX_CHARACTERS[] = "0123456789ABCDEF";
-// #define BIT_WIDE_HEX 8
+// int x2str(char *buf, unsigned num){
+// 	char xtab[] = "0123456789abcdef";
+// 	int ret = 0;
+// 	char *p = buf;
+// 	char *q = buf;
+// 	while(num > 15)
+// 	{
+// 		*q++ = xtab[num%16];
+// 		ret++;
+// 		num /= 16;
+// 	}			
+// 	*q++ = xtab[num%16];
+// 	ret++;
+// 	*q = '\0';
+// 	q--;
+// 	while(q > p){
+// 		char temp = *p;
+// 		*p++ = *q;
+// 		*q-- = temp;
+// 	}
+// 	return ret;
+// }
+
+// int str2str(char *buf, char *src){
+
+// 	strcat(buf, src);
+// 	return strlen(src);
+// }
+
+// int float2str(char *buf, float flt, int len){
+// 	int ret = 0;
+// 	if (flt < 0){
+// 		ret++;
+// 		*buf++ = '-';
+// 	}
+// 	int flt_int = (int)flt;
+// 	float flt_frac = flt - flt_int;
+// 	int tmp_ret = int2str(buf, flt_int);
+// 	ret += tmp_ret;
+// 	buf += tmp_ret;
+// 	ret ++;
+// 	*buf++ = '.';
+// 	while(len--){
+// 		flt_frac *= 10;
+// 	}
+// 	int flt_frac_int = (int)flt_frac;
+// 	ret += int2str(buf, flt_frac_int);
+// 	return ret;
+
+// }
+
 
 // int vsprintf(char *out, const char *fmt, va_list ap) {
-//   return vsnprintf(out, -1, fmt, ap);
+// 	/* panic("Not implemented"); */
+
+// 	*out = '\0';
+// 	int ret = 0;
+// 	while(*fmt != '\0'){
+// 		if(*fmt != '%'){
+// 			ret++;
+// 			*out++ = *fmt++;
+// 			*out = '\0';
+// 		}
+// 		else{
+// 			fmt++;
+// 			switch(*fmt){
+// 				case 's':{	 char *valstr = va_arg(ap, char*);
+// 							 int sub_ret = str2str(out, valstr);
+// 							 ret += sub_ret;
+// 							 out += sub_ret;
+// 							 fmt++;
+// 							 assert(*out=='\0');
+// 							 break;
+// 						 }
+// 				case 'd':{	 int valint = va_arg(ap, int);
+// 							 int sub_ret = int2str(out, valint);
+// 							 ret += sub_ret;
+// 							 out += sub_ret;
+// 							 fmt++;
+// 							 break;
+// 						 }
+// 				/* case 'f':{ */	 
+// 				/* 			 float valflt  = va_arg(ap, double); */
+// 				/* 			 int sub_ret = float2str(out, valflt, 6); */
+// 				/* 			 ret += sub_ret; */
+// 				/* 			 out += sub_ret; */
+// 				/* 			 fmt++; */
+// 				/* 			 /1* *out = '\0'; *1/ */
+// 				/* 			 break; */
+// 				/* 		 } */
+// 				case 'x':{	 
+// 							 int val  = va_arg(ap , int);
+// 							 int sub_ret = x2str(out, val);
+// 							 ret += sub_ret;
+// 							 out += sub_ret;
+// 							 fmt++;
+// 							 /* *out = '\0'; */
+// 							 break;
+// 						 }
+// 				case 'p':{	 
+// 							 ret += 2;
+// 							 *out++ = '0';
+// 							 *out++ = 'x';
+// 							 int val  = va_arg(ap , int);
+// 							 char addr[10]="\0";
+// 							 int sub_ret = x2str(addr, val);
+// 							 for(int k = 8-sub_ret; k > 0; k--){
+// 								*out++ = '0';
+// 							 }
+// 							 *out = '\0';
+// 							 strcat(out, addr);
+// 							 ret += 8;
+// 							 out += sub_ret;
+// 							 fmt++;
+// 							 /* *out = '\0'; */
+// 							 break;
+// 						 }
+// 				case 'c':{
+// 							char val = va_arg(ap, int);
+// 							ret++;
+// 							*out++ = val;
+// 							fmt++;
+// 							break;
+// 						 }
+// 				default: {	 ret++;
+// 							 *out++ = *fmt++;
+// 							 *out = '\0';
+// 						 }
+// 			}
+// 		}
+
+// 		assert(*out == '\0');
+// 	}
+// 	assert(*out == '\0');
+// 	return ret;
 // }
 
-// int sprintf(char *out, const char *fmt, ...) {
-//   va_list valist;
-//   va_start(valist, fmt);
 
-//   int res = vsprintf(out ,fmt, valist);
-//   va_end(valist);
-//   return res;
+// int sprintf(char *out, const char *fmt, ...) {
+// 	/* panic("Not implemented"); */
+// 	va_list ap;
+// 	va_start(ap, fmt);
+// 	int ret = vsprintf(out, fmt, ap);
+// 	va_end(ap);
+// 	return ret;
+// }
+
+
+// int printf(const char *fmt, ...) {
+// 	/* panic("Not implemented"); */
+// 	char out[4095];
+// 	va_list ap;
+// 	va_start(ap, fmt);
+// 	int ret = vsprintf(out, fmt, ap);
+// 	va_end(ap);
+// 	/* putch('A'); */
+// 	/* for(int k = 0; k < 20; k++){ */
+// 	/* putch(out[k]); */
+// 	/* putch('\n'); */
+// 	/* } */
+// 	putstr(out);
+// 	return ret;
 // }
 
 // int snprintf(char *out, size_t n, const char *fmt, ...) {
-//   va_list arg;
-//   va_start (arg, fmt);
-
-//   int done = vsnprintf(out, n, fmt, arg);
-
-//   va_end(arg);
-//   return done;
+// 	panic("Not implemented");
 // }
-
-// #define append(x) {out[j++]=x; if (j >= n) {break;}}
 
 // int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-//   char buffer[128];
-//   // char cha;
-//   char *txt, cha;
-//   // int len;
-//   int num, len;
-//   unsigned int unum;
-//   uint32_t pointer;
-  
-  
-//   int state = 0, i, j;//模仿一个状态机
-//   for (i = 0, j = 0; fmt[i] != '\0'; ++i){
-//     switch (state)
-//     {
-//     case 0:
-//       if (fmt[i] != '%'){
-//         append(fmt[i]);
-//       } else
-//         state = 1;
-//       break;
-    
-//     case 1:
-//       switch (fmt[i])
-//       {
-//       case 's':
-//         txt = va_arg(ap, char*);
-//         for (int k = 0; txt[k] !='\0'; ++k)
-//           append(txt[k]);
-//         break;
-      
-//       case 'd':
-//         num = va_arg(ap, int);
-//         if(num == 0){
-//           append('0');
-//           break;
-//         }
-//         if (num < 0){
-//           append('-');
-//           num = 0 - num;
-//         }
-//         for (len = 0; num ; num /= 10, ++len)
-//           //buffer[len] = num % 10 + '0';//逆序的
-//           buffer[len] = HEX_CHARACTERS[num % 10];//逆序的
-//         for (int k = len - 1; k >= 0; --k)
-//           append(buffer[k]);
-//         break;
-      
-//       case 'c':
-//         cha = (char)va_arg(ap, int);
-//         append(cha);
-//         break;
-
-//       case 'p':
-//         pointer = va_arg(ap, uint32_t);
-//         for (len = 0; pointer ; pointer /= 16, ++len)
-//           buffer[len] = HEX_CHARACTERS[pointer % 16];//逆序的
-//         for (int k = 0; k < BIT_WIDE_HEX - len; ++k)
-//           append('0');
-
-//         for (int k = len - 1; k >= 0; --k)
-//           append(buffer[k]);
-//         break;
-
-//       case 'x':
-//         unum = va_arg(ap, unsigned int);
-//         if(unum == 0){
-//           append('0');
-//           break;
-//         }
-//         for (len = 0; unum ; unum >>= 4, ++len)
-//           buffer[len] = HEX_CHARACTERS[unum & 0xF];//逆序的
-
-//         for (int k = len - 1; k >= 0; --k)
-//           append(buffer[k]);
-//         break;  
-
-//       default:
-//         assert(0);
-//       }
-//       state = 0;
-//       break;
-      
-//     }
-//   }
-
-//   out[j] = '\0';
-//   return j;
+// 	panic("Not implemented");
 // }
+
+
+/*TODO:fix this biggggggg BUG!!!*/
+int vsprintf(char *out, const char *fmt, va_list ap);
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap);
+
+int printf(const char *fmt, ...) {
+  char buffer[2048];
+  va_list arg;
+  va_start (arg, fmt);
+  
+  int done = vsprintf(buffer, fmt, arg);
+
+  putstr(buffer);
+
+  va_end(arg);
+  return done;
+}
+
+static char HEX_CHARACTERS[] = "0123456789ABCDEF";
+#define BIT_WIDE_HEX 8
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  return vsnprintf(out, -1, fmt, ap);
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list valist;
+  va_start(valist, fmt);
+
+  int res = vsprintf(out ,fmt, valist);
+  va_end(valist);
+  return res;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  va_list arg;
+  va_start (arg, fmt);
+
+  int done = vsnprintf(out, n, fmt, arg);
+
+  va_end(arg);
+  return done;
+}
+
+#define append(x) {out[j++]=x; if (j >= n) {break;}}
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  char buffer[128];
+  // char cha;
+  char *txt, cha;
+  // int len;
+  int num, len;
+  unsigned int unum;
+  uint32_t pointer;
+  
+  
+  int state = 0, i, j;//模仿一个状态机
+  for (i = 0, j = 0; fmt[i] != '\0'; ++i){
+    switch (state)
+    {
+    case 0:
+      if (fmt[i] != '%'){
+        append(fmt[i]);
+      } else
+        state = 1;
+      break;
+    
+    case 1:
+      switch (fmt[i])
+      {
+      case 's':
+        txt = va_arg(ap, char*);
+        for (int k = 0; txt[k] !='\0'; ++k)
+          append(txt[k]);
+        break;
+      
+      case 'd':
+        num = va_arg(ap, int);
+        if(num == 0){
+          append('0');
+          break;
+        }
+        if (num < 0){
+          append('-');
+          num = 0 - num;
+        }
+        for (len = 0; num ; num /= 10, ++len)
+          //buffer[len] = num % 10 + '0';//逆序的
+          buffer[len] = HEX_CHARACTERS[num % 10];//逆序的
+        for (int k = len - 1; k >= 0; --k)
+          append(buffer[k]);
+        break;
+      
+      case 'c':
+        cha = (char)va_arg(ap, int);
+        append(cha);
+        break;
+
+      case 'p':
+        pointer = va_arg(ap, uint32_t);
+        for (len = 0; pointer ; pointer /= 16, ++len)
+          buffer[len] = HEX_CHARACTERS[pointer % 16];//逆序的
+        for (int k = 0; k < BIT_WIDE_HEX - len; ++k)
+          append('0');
+
+        for (int k = len - 1; k >= 0; --k)
+          append(buffer[k]);
+        break;
+
+      case 'x':
+        unum = va_arg(ap, unsigned int);
+        if(unum == 0){
+          append('0');
+          break;
+        }
+        for (len = 0; unum ; unum >>= 4, ++len)
+          buffer[len] = HEX_CHARACTERS[unum & 0xF];//逆序的
+
+        for (int k = len - 1; k >= 0; --k)
+          append(buffer[k]);
+        break;  
+
+      default:
+        assert(0);
+      }
+      state = 0;
+      break;
+      
+    }
+  }
+
+  out[j] = '\0';
+  return j;
+}
 
 #endif
 
