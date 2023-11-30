b5a859a5e19b5488b99c6a74f3bc0b45d58530b9
tracer-ics2023 <tracer@njuics.org>
2023-11-30 20:08:51 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:51 up 26 min,  3 users,  load average: 0.79, 0.88, 0.71

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
-      printf("event_yield");break;
+      printf("event_yield\n");break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
