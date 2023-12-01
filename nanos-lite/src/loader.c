ee9183862f9c0839354e68121cf67156117c7720
tracer-ics2023 <tracer@njuics.org>
2023-12-02 00:03:50 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:03:50 up 54 min,  3 users,  load average: 0.60, 0.62, 0.58

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -101,7 +101,7 @@ uintptr_t elf_loader(uintptr_t file_off, bool *suc)
 
 
       if(ProAddr == 0 && program_cnt == 1){
-        ProAddr = (uintptr_t)VirtAddr + 0xb4;
+        ProAddr = (uintptr_t)VirtAddr + 0x3fc;
       }
       uint64_t FileSiz = program->p_filesz;
       uint64_t MemSiz = program->p_memsz;
