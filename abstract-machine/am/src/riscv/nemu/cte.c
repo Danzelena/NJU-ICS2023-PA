7c718411173048c979ddf5e62a7a8f455d06d6b7
tracer-ics2023 <tracer@njuics.org>
2023-12-02 14:28:33 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:28:33 up 40 min,  3 users,  load average: 0.70, 0.78, 0.72

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -25,10 +25,9 @@ Context *__am_irq_handle(Context *c)
     switch (c->mcause)
     {
       // BUG:may have bug!
-
-    
     case -1:
       ev.event = EVENT_YIELD;break;
+    
     default:
       ev.event = EVENT_ERROR;
       break;
