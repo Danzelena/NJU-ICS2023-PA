2633a15fc101559797026c9f039d592e57d00223
tracer-ics2023 <tracer@njuics.org>
2023-12-02 15:51:33 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:51:33 up  2:03,  5 users,  load average: 1.29, 0.94, 0.74

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,7 +6,7 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  // a[1] = c->GPR2;
+  a[1] = c->GPR2;
   // a[2] = c->GPR3;
   // a[3] = c->GPR4;
   // printf("a:(%d,%d,%d)\n",a[1],a[2],a[3]);
@@ -18,7 +18,9 @@ void do_syscall(Context *c) {
       // int val = 0;
       // asm volatile("li (GPRx), %0");
       c->GPRx = 0;
-      
+    case SYS_exit:
+      // status: GPR2
+      halt(a[1]);
       break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
