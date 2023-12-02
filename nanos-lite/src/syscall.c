dd40595a3c31bb570b80506102223a71aa778de1
tracer-ics2023 <tracer@njuics.org>
2023-12-02 16:15:02 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:15:02 up  2:26,  5 users,  load average: 0.55, 0.70, 0.72

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,7 +9,7 @@ void do_syscall(Context *c) {
   a[1] = c->GPR2;
   // a[2] = c->GPR3;
   // a[3] = c->GPR4;
-  printf("a:(%d,%d,%d)\n",a[1]);
+  printf("a:(%d)\n",a[1]);
 
   //TODO: do something depend on a[0]
   switch (a[0]) {
