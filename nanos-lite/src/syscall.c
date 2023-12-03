cfe232a2428d37a16413be7346c8f289ecb2b911
tracer-ics2023 <tracer@njuics.org>
2023-12-03 13:29:07 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:29:07 up  1:05,  4 users,  load average: 1.11, 1.11, 0.85

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -25,13 +25,13 @@ void do_syscall(Context *c)
     // break;
     //  TODO:check `fd` value
     int fd = a[1];
-    char *buf = (char*)a[2];
+    // char *buf = (char*)a[2];
     int len = a[3];
     // printf("buf = %s,len=%d\n",buf,len);
     // stdout, stderr
     if(fd == 1 || fd == 2){
       for(int i = 0;i < len;i++){
-        putch(buf[i]);
+        putch(*(((char *)c->GPR3) + i));
       }
     }
     else{
