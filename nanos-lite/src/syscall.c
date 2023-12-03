ebc67a72513c51bf963f6b33a9ba64438929fd03
tracer-ics2023 <tracer@njuics.org>
2023-12-03 13:25:07 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:25:07 up  1:01,  4 users,  load average: 1.04, 1.19, 0.80

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,9 +1,9 @@
 #include <common.h>
 #include "syscall.h"
 
-
 // This is a user_handler func for AM, will handle different syscall
-void do_syscall(Context *c) {
+void do_syscall(Context *c)
+{
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
@@ -11,48 +11,56 @@ void do_syscall(Context *c) {
   a[3] = c->GPR4;
   // printf("a:(%d)\n",a[1]);
 
-  switch (a[0]) {
-    case SYS_write:
+  switch (a[0])
+  {
+  case SYS_write:
+    if (c->GPR2 == 1 || c->GPR2 == 2)
+    {
+      for (int i = 0; i < c->GPR4; ++i)
+      {
+        putch(*(((char *)c->GPR3) + i));
+      }
+      c->GPRx = c->GPR4;
+    }
+    break;
     //  TODO:check `fd` value
-      int fd = a[1];
-      char *buf = (char*)a[2];
-      int len = a[3];
-      printf("buf = %s,len=%d\n",buf,len);
-      // stdout, stderr
-      if(fd == 1 || fd == 2){
-        for(int i = 0;i < len;i++){
-          putch(buf[i]);
-        }
-      }
-      else{
-        putch('S');putch('o');putch('r');putch('r');putch('y');putch('t');putch('o');putch('W');putch('r');putch('\n');
-        panic("Sorry,fd is not stdout or stderr\n");
-      }
-      c->GPRx = 0;
-      break;
+    // int fd = a[1];
+    // char *buf = (char*)a[2];
+    // int len = a[3];
+    // printf("buf = %s,len=%d\n",buf,len);
+    // // stdout, stderr
+    // if(fd == 1 || fd == 2){
+    //   for(int i = 0;i < len;i++){
+    //     putch(buf[i]);
+    //   }
+    // }
+    // else{
+    //   putch('S');putch('o');putch('r');putch('r');putch('y');putch('t');putch('o');putch('W');putch('r');putch('\n');
+    //   panic("Sorry,fd is not stdout or stderr\n");
+    // }
+    // c->GPRx = 0;
+    // break;
 
+  case SYS_brk:
+    // uintptr_t addr = a[1];
+    c->GPRx = 0;
+    break;
+  case SYS_yield:
+    yield();
+    // int val = 0;
+    // int val =0;
+    // asm volatile("li a0, 123" : : "r"(val));
+    c->GPRx = 0;
+    // panic("a0:%d",c->GPRx);
+    break;
+  case SYS_exit:
+    // printf("exit!Ohhh\n");
+    // status: GPR2
 
-    case SYS_brk:
-      // uintptr_t addr = a[1];
-      c->GPRx = 0;
-      break;
-    case SYS_yield:
-      yield();
-      // int val = 0;
-      // int val =0;
-      // asm volatile("li a0, 123" : : "r"(val));
-      c->GPRx = 0;
-      // panic("a0:%d",c->GPRx);
-      break;
-    case SYS_exit:
-      // printf("exit!Ohhh\n");
-      // status: GPR2
+    halt(a[1]);
+    break;
 
-
-  
-      halt(a[1]);
-      break;
-
-    default: panic("Unhandled syscall ID = %d", a[0]);
+  default:
+    panic("Unhandled syscall ID = %d", a[0]);
   }
 }
