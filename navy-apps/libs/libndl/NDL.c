6b85b5483ec906822f0b93311ea01adc05127281
tracer-ics2023 <tracer@njuics.org>
2023-12-08 22:34:00 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:34:00 up 7 min,  1 user,  load average: 1.61, 1.09, 0.58

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -61,7 +61,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   // int fd = open("/dev/fb", 0);
   // for (int i = 0;i < h;i ++){
-  //   lseek(fd,((y + i)*screen_h + x)*sizeof(uint32_t),SEEK_SET);
+  //   lseek(fd,((y + i)*screen_w + x)*sizeof(uint32_t),SEEK_SET);
   //   int s = write(fd,pixels + w * i,w*sizeof(uint32_t));
   // }
   int graphics = open("/dev/fb", 0);
