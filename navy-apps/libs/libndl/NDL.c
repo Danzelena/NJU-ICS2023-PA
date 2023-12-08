0d121cb228edc43b312be1f11de65a0609e2a7f6
tracer-ics2023 <tracer@njuics.org>
2023-12-08 22:16:28 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:16:28 up  2:25,  3 users,  load average: 1.83, 1.29, 1.04

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -59,10 +59,16 @@ void NDL_OpenCanvas(int *w, int *h) {
 // 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
 // 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  int fd = open("/dev/fb", 0);
-  for (int i = 0;i < h;i ++){
-    lseek(fd,((y + i)*screen_h + x)*sizeof(uint32_t),SEEK_SET);
-    int s = write(fd,pixels + w * i,w*sizeof(uint32_t));
+  // int fd = open("/dev/fb", 0);
+  // for (int i = 0;i < h;i ++){
+  //   lseek(fd,((y + i)*screen_h + x)*sizeof(uint32_t),SEEK_SET);
+  //   int s = write(fd,pixels + w * i,w*sizeof(uint32_t));
+  // }
+    int graphics = open("/dev/fb", 0);
+  canvas_x,canvas_y = 0,0;
+  for (int i = 0; i < h; ++i){
+    lseek(graphics, ((canvas_y + y + i) * screen_w + (canvas_x + x)) * sizeof(uint32_t), SEEK_SET);
+    ssize_t s = write(graphics, pixels + w * i, w * sizeof(uint32_t));
   }
   close(fd);
 }
