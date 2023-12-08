893a402e8715fae270473f081fe1dc2f39d857cc
tracer-ics2023 <tracer@njuics.org>
2023-12-08 16:12:08 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:08 up  1:39,  5 users,  load average: 0.84, 0.94, 0.97

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -6,15 +6,16 @@ void hello_world(){
 int main(){
     printf("Debug:target1\n");
     struct timeval tv;
-    int cnt = 1;
+    int cnt = 0;
     printf("Debug:target2\n");
     while(1){
         gettimeofday(&tv,NULL);
-        printf("Debug:target3\n");
-        printf("tv_usec=%d",tv.tv_usec);
-        while(tv.tv_usec < 500000){
+        // printf("Debug:target3\n");
+        // printf("tv_usec=%d",tv.tv_usec);
+        while(tv.tv_usec+tv.tv_sec*1000000 < 500000*cnt){
             gettimeofday(&tv,NULL);
         }
+        
         hello_world();
         cnt++;
     }
