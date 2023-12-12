0ef3b899ead351350ab31ac916192ab474695a1b
tracer-ics2023 <tracer@njuics.org>
2023-12-12 19:10:17 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:10:17 up  2:12,  3 users,  load average: 1.08, 0.67, 0.56

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  char *filename = "/bin/nterm";
+  char *filename = "/bin/nslider";
   naive_uload(NULL,filename);
 }
 
