8427a58e907d34bdbff8ef6f2d3ef823a5cef261
tracer-ics2023 <tracer@njuics.org>
2024-01-09 21:19:38 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:19:38 up 59 min,  4 users,  load average: 1.45, 1.13, 0.97

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -4,6 +4,9 @@
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
+
+// 解析出 argc, argv, envp, 並且調用 main
+// args -> argc 的地址
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
