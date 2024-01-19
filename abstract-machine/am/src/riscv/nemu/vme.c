328bb94c6bcfb3b6d4a16842f538f6a034bd0159
tracer-ics2023 <tracer@njuics.org>
2024-01-19 22:31:46 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:31:46 up  1:29,  3 users,  load average: 0.76, 0.87, 0.99

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -44,12 +44,13 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   int i;
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
+    printf("va_begin=%x\n", va);
     for (; va < segments[i].end; va += PGSIZE) {
       map(&kas, va, va, 0);
       
       
     }
-    printf("va=%x\n", va);
+    printf("va_end=%x\n", va);
     
   }
 
