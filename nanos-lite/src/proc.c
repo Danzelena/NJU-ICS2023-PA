7bf3134a7a1a2b770c9ddba805f6e4087d5e386e
tracer-ics2023 <tracer@njuics.org>
2024-01-21 00:49:46 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:49:45 up 48 min,  3 users,  load average: 1.32, 0.86, 0.78

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -237,6 +237,8 @@ Context* schedule(Context *prev) {
   printf("(pcb0)pdir=%x, (pcb1)pdir=%x\n", pcb[0].cp->pdir, pcb[1].cp->pdir);
   if(current==&pcb[0]){printf("(Debug)(Schedule)go to 0\n");}
   if(current==&pcb[1]){printf("(Debug)(Schedule)go to 1\n");}
+  printf("(Schedule)max_brk[0]=%x, [1]=%x\n", pcb[0].max_brk, pcb[1].max_brk);
+
   // printf("(Debug)entry=0x%x\n", current->cp->uc.uc_mcontext.gregs[REG_RIP]);
 
   return current->cp;
