195d29f81cfddc1d88727921863f9e740892cdb7
tracer-ics2023 <tracer@njuics.org>
2024-01-21 01:08:28 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:08:28 up  1:06,  3 users,  load average: 0.82, 0.92, 0.81

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,8 +22,9 @@ void hello_fun(void *arg) {
   
   while (1) {
     // printf("max_brk[0]=%x, [1]=%x\n", pcb[0].max_brk, pcb[1].max_brk);
-    Log("Hello World from Nanos-lite with arg '%x' for the %dth time!", (uintptr_t)arg, j);
-    printf("666666666666666666\n");
+    // Log("Hello World from Nanos-lite with arg '%x' for the %dth time!", (uintptr_t)arg, j);
+    Log("Hello world");
+    // printf("666666666666666666\n");
     j ++;
     // printf("max_brk[0]=%x, [1]=%x\n", pcb[0].max_brk, pcb[1].max_brk);
     for (int volatile i = 0; i < HELLO_RATE; i++) ;// make it slower
