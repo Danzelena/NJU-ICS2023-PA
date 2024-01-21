e1cbbf4970d86d87323d8b4957f5a401f972f691
tracer-ics2023 <tracer@njuics.org>
2024-01-21 19:03:35 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:03:35 up  2:19,  3 users,  load average: 0.61, 0.65, 0.73

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -244,7 +244,7 @@ Context* schedule(Context *prev) {
   // printf("(DEBUG)pcb[1]->as->pdir=%x\n", pcb[1].as.ptr);
   // printf("(DEBUG)pcb[0]->pdir=%x, mstatus=%x\n", pcb[0].cp->pdir, pcb[0].cp->mstatus);
   // printf("(DEBUG)pcb[1]->pdir=%x, mstatus=%x\n", pcb[1].cp->pdir, pcb[1].cp->mstatus);
-  printf("go to %d\n", current == &pcb[0]? 0:1);
+  
   // assert(pcb[0].cp->pdir == NULL);
   
 
@@ -254,6 +254,7 @@ Context* schedule(Context *prev) {
     current->cp->pdir = current->as.ptr;
     printf("(DEBUG)pdir=%x, ptr=%x\n",current->cp->pdir,current->as.ptr);
   }
+  printf("go to %d\n", current == &pcb[0]? 0:1);
   assert(current->cp->pdir == current->as.ptr);
   return current->cp;
 }
