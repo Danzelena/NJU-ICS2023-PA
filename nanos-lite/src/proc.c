392939367b289c65419e08b9212317837e3d650f
tracer-ics2023 <tracer@njuics.org>
2024-01-22 21:15:40 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:15:40 up 35 min,  3 users,  load average: 1.35, 1.39, 1.13

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -216,19 +216,19 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 }
 void init_proc() {
   // naive_uload(&pcb[0],"/bin/dummy");
-  context_kload(&pcb[0], hello_fun, (void *)1L);
-  // context_uload(&pcb[0], "/bin/dummy", NULL, NULL);
+  // context_kload(&pcb[0], hello_fun, (void *)1L);
+  context_uload(&pcb[0], "/bin/hello", NULL, NULL);
 
   // BUG: 根据目前计算 argc, envc的方法,必须这么定义 argv, envp
   // char *const argv[] = {"--skip", "hello", "world", "NJU", NULL};
   // char *const envp[] = {"ICS", "PA", "pa", NULL};
 
   // BUG: "nil" for NULL, always make Segemetion fault when getenv()
-  char *const argv[] = {"/bin/exec-test", NULL};
+  // char *const argv[] = {"/bin/exec-test", NULL};
   // char *const envp[] = {"nil", NULL};
 
   // context_uload(&pcb[1], "/bin/menu", NULL, NULL);
-  context_uload(&pcb[1], "/bin/menu", argv, NULL);
+  context_uload(&pcb[1], "/bin/menu", NULL, NULL);
 
 
   // printf("(DEBUG)(init_proc)pcb[0]->pdir=%x, mstatus=%x\n", pcb[0].cp->pdir, pcb[0].cp->mstatus);
