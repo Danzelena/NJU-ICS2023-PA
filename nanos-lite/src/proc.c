dede28b522668afa0a1ab8129cc3d8631b184921
tracer-ics2023 <tracer@njuics.org>
2024-01-22 21:43:30 +0800

    >  compile NEMU 221300089 张淩翔 Linux elenadanz-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:43:30 up  1:02,  3 users,  load average: 1.18, 1.30, 1.18

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -216,8 +216,8 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 }
 void init_proc() {
   // naive_uload(&pcb[0],"/bin/dummy");
-  // context_kload(&pcb[0], hello_fun, (void *)1L);
-  context_uload(&pcb[0], "/bin/hello", NULL, NULL);
+  context_kload(&pcb[0], hello_fun, (void *)1L);
+  // context_uload(&pcb[0], "/bin/hello", NULL, NULL);
 
   char *const argv[] = {"/bin/exec-test", NULL};
   context_uload(&pcb[1], "/bin/exec-test", argv, NULL);
